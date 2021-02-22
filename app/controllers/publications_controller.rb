a2beadab5267f6c68190bd253d6e8457f22f601b
Julien Mathieu <julienmathieu.contact@gmail.com>
2021-02-22 15:51:42 +0100

    coded the publication controller

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,22 +1,39 @@
 class PublicationsController < ApplicationController
   def index
+    @publications = Publication.all
   end
 
   def show
+    @publication = Publication.find(params[:id])
   end
 
   def new
+    @publication = Publication.new
   end
 
   def create
+    @publication = Publication.new(publication_params)
+    @publication.save
+    redirect_to publications_index_path
   end
 
   def edit
+    @publication = Publication.find(params[:id])
   end
 
   def update
+    @publication = Publication.find(params[:id])
+    @publication.update(publication_params)
+    redirect_to publications_path
   end
 
   def destroy
+    @publication = Publication.find(params[:id])
+    @publication.destroy
+    redirect_to publications_path
+  end
+
+  def publication_params
+    params.require(:publication).permit(:title, :user)
   end
 end
