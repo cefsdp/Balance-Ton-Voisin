c64fa52236fefe7ab8770a2c2813452a43ed6462
Charles Fousse <cefsdp@gmail.com>
2021-02-24 14:31:30 +0100

    added edit and destroy btn to publication show

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,6 +1,6 @@
 class Publication < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   #validates :content, presence: true
   #validates :content, uniqueness: true
