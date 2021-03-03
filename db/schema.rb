e9c8b5fa1128262d6a33d062cd3dd13ad30c0207
Julien Mathieu <julienmathieu.contact@gmail.com>
2021-03-03 09:42:47 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_085139) do
+ActiveRecord::Schema.define(version: 2021_03_02_152828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,16 @@ ActiveRecord::Schema.define(version: 2021_03_02_085139) do
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
+  create_table "notifications", force: :cascade do |t|
+    t.string "notification_type", null: false
+    t.jsonb "params"
+    t.datetime "read_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_notifications_on_user_id"
+  end
+
   create_table "publications", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -104,6 +114,7 @@ ActiveRecord::Schema.define(version: 2021_03_02_085139) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "publications"
   add_foreign_key "comments", "users"
+  add_foreign_key "notifications", "users"
   add_foreign_key "publications", "users"
   add_foreign_key "votes", "clashes"
   add_foreign_key "votes", "users"
